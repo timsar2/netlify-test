a31f572dcb66964394d55be538779406b5270ff7
timsar2 <info.golab@gmail.com>
2025-03-06 17:15:29 +0330

    .

--- projects/shared/src/lib/shared.component.ts
+++ projects/shared/src/lib/shared.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'lib-shared',
   imports: [],
   template: `
     <p>
-      shared works!
+      shared works! counter is: {{counter()}}
     </p>
   `,
   styles: ``
 })
 export class SharedComponent {
-
+  counter = signal<number>(0)
+  constructor(){
+    setInterval(() => {
+      this.counter.set(this.counter() + 1)
+    }, 1000)
+  }
 }
